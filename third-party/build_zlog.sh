711420b3d2368d92c9446396401092607fea45ea
jobs-github <yao050421103@163.com>
2019-08-08 17:42:55 +0800

    update build_zlog.sh

--- third-party/build_zlog.sh
+++ third-party/build_zlog.sh
@@ -4,6 +4,6 @@ test -d zlog-1.2.12 && rm -fr zlog-1.2.12
 tar -zxvf zlog-1.2.12.tar.gz
 cd zlog-1.2.12
 make PREFIX=/usr/local
-sudo make install
+make install
 cd ..
-sudo rm -fr zlog-1.2.12
+rm -fr zlog-1.2.12
