cf6fec3a94279b5d10ab3390e76b4453ea5e70a9
jobs-github <yao050421103@163.com>
2019-08-09 17:58:23 +0800

    add Dockerfile_runtime

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,3 @@
-FROM ubuntu:16.04
+FROM anole:runtime
 
 COPY installation/ /app/
-
-RUN apt-get update \
- && apt-get install -y software-properties-common \
- && apt-get update \
- && add-apt-repository -y ppa:nginx/stable \
- && apt-get update \
- && apt-get install -y nginx \
- && apt-get clean \
- && rm -rf /var/lib/apt/lists/*
--- build_docker.sh
+++ build_docker.sh
@@ -1,1 +1,19 @@
-docker image build -t anole:0.0.1 .
+#!/bin/bash
+
+option=$1
+cwd=$(dirname $(readlink -f $0))
+
+image_runtime="anole:runtime"
+
+function main()
+{
+    if test ! -z "$(docker images -q $image_runtime)"; then
+        echo "$image_runtime exist"
+    else
+        docker image build -f third-party/Dockerfile_runtime -t $image_runtime .
+    fi
+
+    docker image build -t anole:0.0.1 .
+}
+
+main $*
