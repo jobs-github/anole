c17c90a27964651ebe363554a8e95644d164e3d3
jobs-github <yao050421103@163.com>
2019-08-09 15:29:53 +0800

    update build tools

--- make_install.sh
+++ make_install.sh
@@ -31,6 +31,7 @@ function build_3rd()
         docker image build -f Dockerfile_devel -t $image_devel .
     fi
 
+    wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
     docker image build -f Dockerfile_3rd -t $image_3rd_name .
 
     docker run --name $container_3rd_name $image_3rd_name
--- third-party/build_zlog.sh
+++ third-party/build_zlog.sh
@@ -3,7 +3,6 @@
 test -d zlog-1.2.12 && rm -fr zlog-1.2.12
 tar -zxvf zlog-1.2.12.tar.gz
 cd zlog-1.2.12
-make PREFIX=/usr/local
-make install
+make PREFIX=/usr/local/zlog-1.2.12 install
 cd ..
 rm -fr zlog-1.2.12
