cb6286fa58e7022e4e59eedce000bf5682b7d0c1
jobs-github <yao050421103@163.com>
2019-08-12 17:00:59 +0800

    update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run --name anole --restart=always -d -p 80:80 -p 443:443 -v /app/anole/conf:/app/anole/conf  anole:0.0.1 sh -c "/etc/init.d/nginx start && /app/anole/start.sh /app/anole/conf/server.json"
+cmd="/etc/init.d/nginx start && /app/anole/start.sh /app/anole/conf/server.json"
+docker run --name anole --restart=always -d -p 80:80 -p 443:443 \
+    -v /app/anole/conf:/app/anole/conf  \
+    anole:0.0.1 \
+    --mount type=bind,source=/home/$USER,target=/mnt/hgfs/share \
+    sh -c $cmd
