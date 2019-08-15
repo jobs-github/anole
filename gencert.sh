e86def3fa6a29a67d6a5410e1bc643af066b29cb
jobs-github <yao050421103@163.com>
2019-08-15 14:50:08 +0800

    update gencert.sh

--- gencert.sh
+++ gencert.sh
@@ -8,16 +8,20 @@ function main()
     echo "set password (5-10 digit)"
     echo "============================"
     openssl genrsa -des3 -out private.key 1024
+    #openssl genrsa -out private.key 2048
     echo "============================"
     echo "type password you just set"
     echo "============================"
     openssl req -new -subj $SUBJECT -key private.key -out private.csr
+    #openssl req -new -x509 -key private.key -out private.csr -days 3655
     echo "============================"
     echo "type password again"
     echo "============================"
     mv private.key private.or.key
     openssl rsa -in private.or.key -out private.key
     openssl x509 -req -days 3650 -in private.csr -signkey private.key -out private.crt
+    openssl x509 -outform der -in private.csr -out private.der
+    # private.der => ios
 }
 
 main $*
