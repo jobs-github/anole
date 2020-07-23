ca6d1456e85d4fb6f78c776e71676065b09ea4b4
jobs <yao050421103@163.com>
2020-07-23 17:22:48 +0800

    fix bug on decode_domain

--- src/anole_proto.cpp
+++ src/anole_proto.cpp
@@ -138,11 +138,16 @@ int sock5_address_t::decode_ipv4(const std::string& data)
 
 int sock5_address_t::decode_domain(const std::string& data)
 {
-    size_t len = size_t(data[1] + 4);
+    uint8_t domain_len = data[1];
+    if (0 == domain_len)
+    {
+        return -1;
+    }
+    size_t len = size_t(domain_len + 4);
     if (data.size() >= len)
     {
-        address = data.substr(2, data[1]);
-        port = decode_port(data, data[1] + 2);
+        address = data.substr(2, domain_len);
+        port = decode_port(data, domain_len + 2);
         return len;
     }
     return -1;
