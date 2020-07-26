7cc6744f1e394260a1fe3006f673c68b289bda9d
jobs <yao050421103@163.com>
2020-07-26 20:22:05 +0800

    update standalone dep

--- src/make_standalone.sh
+++ src/make_standalone.sh
@@ -4,9 +4,9 @@ function copy_3rd()
 {
     libs=(
         "libboost_system.so.1.66.0 /usr/local/boost_1_66_0/lib/libboost_system.so.1.66.0"
-        "libcrypto.so.1.0.0 /lib/x86_64-linux-gnu/libcrypto.so.1.0.0"
-        "libssl.so.1.0.0 /lib/x86_64-linux-gnu/libssl.so.1.0.0"
-        "libzlog.so.1.1 /usr/local/zlog-1.2.15/lib/libzlog.so.1.1"
+        "libcrypto.so.1.1 /lib/x86_64-linux-gnu/libcrypto.so.1.1"
+        "libssl.so.1.1 /lib/x86_64-linux-gnu/libssl.so.1.1"
+        "libzlog.so.1.2 /usr/local/zlog-1.2.15/lib/libzlog.so.1.2"
     )
 
     for lib in "${libs[@]}"
