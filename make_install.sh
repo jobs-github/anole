2d1d126bc17b3edd40c5a7aa74bd91d228ccc46f
jobs <yao050421103@163.com>
2020-07-26 19:29:09 +0800

    rename docker image

--- make_install.sh
+++ make_install.sh
@@ -3,7 +3,7 @@
 option=$1
 cwd=$(dirname $(readlink -f $0))
 
-image_devel="ubuntu:devel"
+image_devel="ubuntu:20.04.dev"
 image_3rd_name="ubuntu:anole-3rd-build"
 container_3rd_name="anole-3rd-build"
 
